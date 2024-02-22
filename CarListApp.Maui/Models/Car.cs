57d37006498de1e4f705ab95f7709a9ee7e60e3f
Peter Kostov <brucemclaren@mail.bg>
2024-02-22 23:05:00 +0200

    Creating a simple MVVM infrastructure

---
+++ CarListApp.Maui/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListApp.Maui.Models
+{
+    public class Car : BaseEntity
+    {
+        
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Vin { get; set; }
+
+    }
+
+
+}
---
+++ CarListApp.Maui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListApp.Maui.ViewModels
+{
+    public class BaseViewModel:INotifyPropertyChanged
+    {
+        string _title;
+        bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+                {
+                if (_isBusy == value) return;
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
