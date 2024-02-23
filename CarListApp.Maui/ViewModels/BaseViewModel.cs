b438224715352f4d03b95ed46be8796daf7b0450
Peter Kostov <brucemclaren@mail.bg>
2024-02-23 12:18:58 +0200

    Improving the MVVM structure

--- CarListApp.Maui/ViewModels/BaseViewModel.cs
+++ CarListApp.Maui/ViewModels/BaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,34 +10,17 @@ using System.Threading.Tasks;
 
 namespace CarListApp.Maui.ViewModels
 {
-    public class BaseViewModel:INotifyPropertyChanged
+    public partial class BaseViewModel:ObservableObject
     {
-        string _title;
-        bool _isBusy;
-        public bool IsBusy
-        {
-            get => _isBusy;
-            set
-                {
-                if (_isBusy == value) return;
-                _isBusy = value;
-                OnPropertyChanged();
-            }
-        }
-        public string Title
-        {
-            get => _title;
-            set
-            {
-                if (_title == value) return;
-                _title = value;
-                OnPropertyChanged();
-            }
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string name=null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+        [ObservableProperty]
+        string title;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotLoading))]
+        bool isLoading;
+
+
+        public bool IsNotLoading => !isLoading;
+        
     }
 }
